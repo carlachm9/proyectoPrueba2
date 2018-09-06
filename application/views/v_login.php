d14065d6925c2c3b744a08526a572015ba874c59
carlachm9 <carlachm9@gmail.com>
2018-09-06 17:27:12 -0500

    Prueba2

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,9 +1,128 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Proyecto de Prueba 2</title>
+	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+        <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description"            content="Registro de Oportunidades DCN">
+        <meta name="keywords"               content="Registro de Oportunidades DCN">
+        <meta name="robots"                 content="Index,Follow">
+        <meta name="date"                   content="Febrero 15, 2018"/>
+        <meta name="language"               content="es">
+        <meta name="theme-color"            content="#000000">
+	<title>HPE Simplivity - Intel</title>
+	<link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/logo_favicon.ico">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/css/bootstrap.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>metric.css?v=<?php echo time();?>">
+	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
 </head>
 <body>
-	<h2>Proyecto Prueba 2</h2>
+	<div class="header">
+		<div class="header-container">
+			<div class="header-container--left">
+				<img src="<?php echo RUTA_IMG?>logo/hpe-intel.png">
+			</div>
+			<div class="header-container--right">
+				<p>Si tiene Intel&reg;, tiene una productividad poderosa</p>
+			</div>
+		</div>
+	</div>
+	<section id="home">
+		<div class="js-fondo"></div>
+		<div class="js-container">
+			<div class="js-home js-flex">
+				<div class="js-contenido">
+					<h2>HPE Simplivity: Entrenamiento Técnico para Partners</h2>
+					<p>Taller ofrecido por HPE con el patrocinio de Intel®</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="register" class="js-section">
+		<div class="js-container">
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+					<div class="js-input js-select">
+						<select class="selectpicker">
+		  					<option>Bogotá</option>
+		  					<option>Buenos Aires</option>
+		  					<option>Ciudad de Mexico</option>
+		  					<option>Santiago</option>
+	 						<option>Sao Paulo</option>
+						</select>						
+					</div>
+				</div>
+			</div>
+			<div class="js-agenda">
+			</div>
+			<div class="js-title">
+				<h2>Regístrese completando el siguiente formulario</h2>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Nombre*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Apellido*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Email*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Telefono*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Nombre de Canal*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Cargo*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="js-input">
+						<label for="text">Pais*</label>
+						<input type="text" class="form-control" placeholder="" aria-describedby="sizing-addon1">
+					</div>
+				</div>
+			</div>
+			<div class="js-section--button text-left">
+				<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect js-button">Enviar</button>
+			</div>
+		</div>
+	</section>
+	<footer class="js-section">
+		<div class="js-container text-center">
+			<p>Intel y el logotipo de Intel son marcas comerciales de la Corporación Intel o sus filiales en los Estados Unidos o en otros países</p>
+			<p>©2018 Copyright Hewlett Packard Enterprise Development LP</p>
+		</div>
+	</footer>
+	
+	
 </body>
+	<script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+	<script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+	<script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+	<script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+	<script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+	<script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jsindex.js?v=<?php echo time();?>"></script>
 </html>
